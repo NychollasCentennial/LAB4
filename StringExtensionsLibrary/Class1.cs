ef9ebfad41e9427b797dd4d7094249033327258f
NychollasCentennial <ndellamo@my.centennialcollege.ca>
2024-12-01 03:52:48 -0500

    Initial commit

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            char firstChar = input[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+}
